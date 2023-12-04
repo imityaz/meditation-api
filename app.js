4c0e9f9a8d0497d8cbd990bbc9c3b9289717c466
Devex Hub <developmentexpert121@gmail.com>
2023-12-04 09:18:58 +0530

    update cors url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const userRoutes = require("./routes/userRoutes")
 const communityRoutes = require("./routes/communityRoutes")
 app.use(cors({
-    origin: 'https://meditation-12aaf.web.app',
+    origin: 'https://meditation-12aaf.firebaseapp.com',
     optionsSuccessStatus: 200,
   }));
 app.use("/api/user", userRoutes);
